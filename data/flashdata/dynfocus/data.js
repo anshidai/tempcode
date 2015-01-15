8b08ce72089902058fbb4979df9a28e90f6e525a
an_shidai <287639598@qq.com>
2015-01-15 18:59:25 +0800

    add dir

---
+++ data/flashdata/dynfocus/data.js
@@ -0,0 +1,13 @@
+imgUrl1="data/afficheimg/20141013ygxtlp.jpg";
+imgtext1="";
+imgLink1=escape("http://www.temperqueen.com/Goods-A-line-Brown-Silver-Sweep-Brush-Train-Buttons-High-Neck-Zipper-Satin-Long-Sleeve-Pear-g1744.html");
+imgUrl2="data/afficheimg/20141013oatkwx.jpg";
+imgtext2="";
+imgLink2=escape("http://www.temperqueen.com/Goods-Trumpet-Mermaid-Green-Brown-Sweep-Brush-Train-Tiers-Sweetheart-Backless-Organza-Sleeveless-Hourglass-g1727.html");
+imgUrl3="data/afficheimg/20141013oxtxtj.jpg";
+imgtext3="";
+imgLink3=escape("http://www.temperqueen.com/Goods-Sheath-Column-Grey-Silver-Sweep-Brush-Train-Appliques-V-neck-Backless-Tulle-Sleeveless-Pear-g1717.html");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
